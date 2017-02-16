dbc2909458e0f557219b9bd67c08471d3509f7ba
Prashanth Murali <prashanthkumar374@gmail.com>
2017-02-16 17:52:44 -0500

    Adding panel and action list

--- action_list_sample/index.android.js
+++ action_list_sample/index.android.js
@@ -10,10 +10,12 @@ import {
   StyleSheet,
   ListView,
   Text,
+    ScrollView,
   View
 } from 'react-native';
 import Header from './header';
 import ActionList from './action_list';
+import Panel from './panel';
 
 export default class action_list_sample extends Component {
 
@@ -21,8 +23,23 @@ export default class action_list_sample extends Component {
     return (
       <View>
       <Header />
+        <View>
+        <ScrollView style={styles.container}>
 
-      <ActionList />
+          <Panel title="A Panel with short content text">
+            <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
+          </Panel>
+
+          <Panel title="A Panel with long content text">
+           <ActionList />
+          </Panel>
+
+          <Panel title="Another Panel">
+            <Text>Lorem ipsum dolor sit amet...</Text>
+          </Panel>
+
+        </ScrollView>
+        </View>  
       </View>
     );
   }
@@ -30,10 +47,8 @@ export default class action_list_sample extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor : '#f4f7f9',
+    paddingTop      : 30
   },
   welcome: {
     fontSize: 20,
