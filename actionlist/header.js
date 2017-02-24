9215ef6373a942654b1436dee04645249bb72ae0
necsashi <necsashi@indiana.edu>
2017-02-24 00:20:53 -0500

    Lint issues resolved

---
+++ actionlist/header.js
@@ -0,0 +1,38 @@
+// eslint-disable-line global-require
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from 'react-native';
+
+const style = StyleSheet.create({
+  container: {
+    backgroundColor: '#7a1705',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  text: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 18,
+    padding: 20,
+  },
+
+  image: {
+    width: 50,
+    height: 50,
+  },
+});
+
+/* eslint-disable global-require */
+const Header = () => (
+  <View style={style.container}>
+    <Image style={style.image} source={require('./img/trident-large.png')} />
+    <Text style={style.text}>Welcome to Action List</Text>
+  </View>
+  );
+
+module.exports = Header;
---
+++ actionlist/index.android.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  ScrollView,
+  View,
+} from 'react-native';
+
+import ActionListItems from './action_list';
+import Panel from './panel';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f4f7f9',
+    paddingTop: 30,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+const Header = require('./header');
+
+const ActionList = () => (
+  <View>
+    <Header />
+    <View>
+      <ScrollView style={styles.container}>
+        <Panel title="A Panel with short content text">
+          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
+        </Panel>
+        <Panel title="A Panel with long content text">
+          <ActionListItems />
+        </Panel>
+        <Panel title="Another Panel">
+          <Text>Lorem ipsum dolor sit amet...</Text>
+        </Panel>
+      </ScrollView>
+    </View>
+  </View>
+);
+
+AppRegistry.registerComponent('ActionList', () => ActionList);
