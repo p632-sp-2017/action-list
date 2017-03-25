1d16565a2c478b3e4722fb69e34b9322a95ae6b2
necsashi <necsashi@indiana.edu>
2017-03-25 17:09:11 -0400

    Sidemenu added

--- actionlist/app/components/display_list.js
+++ actionlist/app/components/display_list.js
@@ -7,6 +7,7 @@ import {
 } from 'react-native';
 import RenderList from './render_list';
 
+
 const styles = StyleSheet.create({
   full_container: {
     backgroundColor: '#ffffff',
--- actionlist/app/containers/ActionListContainer.js
+++ actionlist/app/containers/ActionListContainer.js
@@ -1,26 +1,113 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-// import { ActionCreators } from '../actions';
-
 import {
+  StyleSheet,
   AppRegistry,
   View,
+  Text,
+  Image,
+  TouchableHighlight,
+  DrawerLayoutAndroid,
 } from 'react-native';
 
-import Header from '../components/header';
+import sidemenu_press from '../actions/action_items';
+import SideMenu from '../components/SideMenu.js'
 import DisplayList from '../components/display_list';
 
-const ActionListContainer = () => (
-  <View>
-    <Header />
-    <DisplayList />
-  </View>
-);
+const style = StyleSheet.create({
+  toolbar: {
+    backgroundColor: '#7a1705',
+    borderTopColor: '#990000',
+    borderTopWidth: 5,
+    flexDirection: 'row',
+  },
+  toolbarTrident: {
+    width: 43,
+    height: 50,
+    marginLeft: 10,
+  },
+  toolbarTitle: {
+    color: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontFamily: 'BentonSansBold, Arial, sans-serif',
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginLeft: 10,
+    marginTop: 7,
+    flex: 1,
+  },
+  toolbarSearch: {
+    margin: 15,
+    width: 16,
+    height: 16,
+  },
+  toolbarButton: {
+    margin: 15,
+    width: 16,
+    height: 16,
+    marginRight: 20,
+    justifyContent: 'flex-end',
+  },
+});
 
-export default ActionListContainer;
 
-// function mapDispatchToProps(dispatch) {
-//   return bindActionCreators(ActionCreators, dispatch);
-// }
-// export default connect(() => { return {} }, mapDispatchToProps)(ActionListContainer);
+export default class ActionListContainer extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      drawerClosed: true,
+    }
+    this.toggleDrawer = this.toggleDrawer.bind(this);
+    this.setDrawerState = this.setDrawerState.bind(this);
+    
+  }
+
+  toggleDrawer() {
+    if (this.state.drawerClosed) {
+      this.DRAWER.openDrawer();
+    } else {
+      this.DRAWER.closeDrawer();
+    }
+  }  
+  setDrawerState() {
+    this.setState({
+      drawerClosed: !this.state.drawerClosed
+    });
+  }
+  render(){
+    return(
+      <DrawerLayoutAndroid
+      drawerWidth={300}
+      ref={(drawerElement) => { this.DRAWER = drawerElement; }}
+      drawerPosition={DrawerLayoutAndroid.positions.Right}
+      onDrawerOpen={this.setDrawerState}
+      onDrawerClose={this.setDrawerState}
+      renderNavigationView={SideMenu}>
+        
+      <View style={style.toolbar}>
+      <Image
+        style={style.toolbarTrident}
+        source={require('../components/img/trident-large.png')}
+      />
+      <Text style={style.toolbarTitle}>
+        IU Action List
+      </Text>
+      <Image
+        style={style.toolbarSearch}
+        source={require('../components/img/search.png')}
+      />
+      <TouchableHighlight onPress={this.toggleDrawer}>
+        <Image
+          style={style.toolbarButton}
+          source={require('../components/img/menu-icon.png')}
+        />
+      </TouchableHighlight>
+      </View>
+      <DisplayList/>
+    </DrawerLayoutAndroid>
+  );
+  }
+}
+
