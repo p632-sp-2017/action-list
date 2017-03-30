092a196076434072586fdd0cbc3d4db00d4fcdd5
Prashanth Murali <prashanthkumar374@gmail.com>
2017-03-30 19:16:09 -0400

    Using a different drawer component which seems more stateless

--- actionlist/app/containers/ActionListContainer.js
+++ actionlist/app/containers/ActionListContainer.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
+import Drawer from 'react-native-drawer';
 import {
   View,
-  DrawerLayoutAndroid,
 } from 'react-native';
 
 import Header from '../components/header';
@@ -10,60 +10,34 @@ import DisplayList from '../components/display_list';
 import SideMenu from '../components/SideMenu';
 import { toggleDrawer } from '../actions/action_items';
 
-class ActionListContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      drawerExpanded: props.drawerExpanded,
-    };
-  }
+const ActionListContainer = ({ drawerExpanded, setDrawerState }) => (
+  <Drawer
+    openDrawerOffset={100}
+    type="overlay"
+    open={drawerExpanded}
+    side={'right'}
+    tapToClose
+    content={<SideMenu />}
+    onClose={() => setDrawerState()}
+  >
+    <View>
+      <Header />
+      <DisplayList />
+    </View>
+  </Drawer>
+);
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      drawerExpanded: nextProps.drawerExpanded,
-    });
-  }
-
-  componentDidUpdate() {
-    if (this.state.drawerExpanded) {
-      this.DRAWER.openDrawer();
-    } else {
-      this.DRAWER.closeDrawer();
-    }
-  }
-
-  render() {
-    return (
-      <DrawerLayoutAndroid
-        ref={(c) => { this.DRAWER = c; }}
-        drawerWidth={300}
-        drawerPosition={DrawerLayoutAndroid.positions.Right}
-        onDrawerClose={this.props.setDrawerState}
-        renderNavigationView={() => SideMenu}
-      >
-        <View>
-          <Header />
-          <DisplayList />
-        </View>
-      </DrawerLayoutAndroid>
-    );
-  }
-}
-
-function mapStateToProps(state) {
-  return {
-    drawerExpanded: state.actionItemsReducer.drawerExpanded,
-  };
-}
+const mapStateToProps = state => ({
+  drawerExpanded: state.actionItemsReducer.drawerExpanded,
+});
 
 const mapDispatchToProps = dispatch => ({
   setDrawerState: () => dispatch(toggleDrawer()),
 });
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(ActionListContainer);
-
 ActionListContainer.propTypes = {
   drawerExpanded: React.PropTypes.bool.isRequired,
   setDrawerState: React.PropTypes.func.isRequired,
 };
+
+export default connect(mapStateToProps, mapDispatchToProps)(ActionListContainer);
